f11253c13ed2fe424a7b31cb933c395045904f8e
lsm <lsm1998.aliyun.com>
2020-07-06 09:26:34 +0800

    3 & 4

---
+++ ch04/mongo.cpp
@@ -0,0 +1,3 @@
+// C语言连接MongoDB
+// https://blog.csdn.net/beyond706/article/details/105326062
+
--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
     cout << "Hello, World!" << endl;
     cout << "你" << "配" << "吗？" << endl;
     cout << 0xFF << endl;
-    ch01();
+    for (int i = 0; i < argc; ++i)
+    {
+        cout << "参数 :" << i << " =" << argv[i] << endl;
+    }
     return 0;
 }
